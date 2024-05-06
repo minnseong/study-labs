2f0c5059fe722ac85b7d5d132d81e0ff89635d66
minseong <kdsvip5@naver.com>
2024-05-06 13:50:35 +0900

    stock-system-concurrency-issue initial commit

---
+++ stock-system-concurrency-issue/src/main/java/labs/minnseong/stocksystemconcurrencyissue/StockSystemConcurrencyIssueApplication.java
@@ -0,0 +1,13 @@
+package labs.minnseong.stocksystemconcurrencyissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSystemConcurrencyIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSystemConcurrencyIssueApplication.class, args);
+	}
+
+}
---
+++ stock-system-concurrency-issue/src/test/java/labs/minnseong/stocksystemconcurrencyissue/StockSystemConcurrencyIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package labs.minnseong.stocksystemconcurrencyissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSystemConcurrencyIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
