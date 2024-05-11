d4dabb9e722997a459c8677e76a9a85f5d4f8d45
minseong <kdsvip5@naver.com>
2024-05-11 19:09:45 +0900

    [stock] Redis 분산락 AOP 적용

---
+++ stock-system-concurrency-issue/src/main/java/labs/minnseong/stocksystemconcurrencyissue/aop/DistributedLockAspect.java
@@ -0,0 +1,54 @@
+package labs.minnseong.stocksystemconcurrencyissue.aop;
+
+import java.lang.reflect.Method;
+import labs.minnseong.stocksystemconcurrencyissue.annotation.DistributedLock;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DistributedLockAspect {
+
+    private static final String REDISSON_LOCK_PREFIX = "LOCK:";
+
+    private final RedissonClient redissonClient;
+    private final AopForTransaction aopForTransaction;
+
+    @Around("@annotation(labs.minnseong.stocksystemconcurrencyissue.annotation.DistributedLock)")
+    public Object lock(ProceedingJoinPoint joinPoint) throws Throwable  {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
+
+        String key = REDISSON_LOCK_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributedLock.key());
+        RLock rLock = redissonClient.getLock(key);
+
+        System.out.println(key);
+
+        try {
+            boolean available = rLock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
+            if (!available) {
+                return false;
+            }
+
+            return aopForTransaction.proceed(joinPoint);
+        } catch (InterruptedException e) {
+            throw new InterruptedException();
+        } finally {
+            try {
+                rLock.unlock();
+            } catch (IllegalMonitorStateException e) {
+                log.info("Redisson Lock Already UnLock serviceName : {}, key : {}", method.getName(), key);
+            }
+        }
+    }
+}
