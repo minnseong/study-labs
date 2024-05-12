f6871b59e2e418659c97092d2f58c0a4680711be
minseong <kdsvip5@naver.com>
2024-05-12 23:44:23 +0900

    [coupon] redis set를 활용하여, 한명의 유저당 하나의 쿠폰만 발급할 수 있도록 제한

--- coupon-system/api/src/main/java/labs/coupon/api/service/ApplyService.java
+++ coupon-system/api/src/main/java/labs/coupon/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package labs.coupon.api.service;
 
 import labs.coupon.api.producer.CouponCreateProducer;
+import labs.coupon.api.repository.AppliedUserRepository;
 import labs.coupon.api.repository.CouponCountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,16 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
 
     private final CouponCountRepository couponCountRepository;
+    private final AppliedUserRepository appliedUserRepository;
     private final CouponCreateProducer couponCreateProducer;
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
         Long count = couponCountRepository.increment();
 
         if (count > 100) {
