26c30b382aeeaf60ab7cca0c72cec0437edf2df8
minseong <kdsvip5@naver.com>
2024-05-14 01:14:00 +0900

    [coupon-consumer] 불필요 클래스 제거 - FailedEvent 제거

--- coupon-system/consumer/src/main/java/labs/coupon/consumer/repository/CouponRepository.java
+++
@@ -1,8 +0,0 @@
-package labs.coupon.consumer.repository;
-
-import labs.coupon.consumer.domain.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-
-}
--- coupon-system/consumer/src/main/java/labs/coupon/consumer/repository/FailedEventRepository.java
+++
@@ -1,8 +0,0 @@
-package labs.coupon.consumer.repository;
-
-import labs.coupon.consumer.domain.FailedEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FailedEventRepository extends JpaRepository<FailedEvent, Long> {
-
-}
