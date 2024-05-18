df886cd1d458d6fa9c6dda8d6be9a506b1637318
minseong <kdsvip5@naver.com>
2024-05-18 23:58:29 +0900

    [java-to-kotlin] userServiceTest 샘플 테스트

--- library-app/src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
+++ library-app/src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -1,5 +1,8 @@
 package com.group.libraryapp.dto.user.request;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class UserCreateRequest {
 
   private String name;
---
+++ library-app/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -0,0 +1,31 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceTest @Autowired constructor(
+    private val userRepository: UserRepository,
+    private val userService: UserService,
+) {
+
+    @Test
+    fun saveUserTest() {
+
+        // given
+        val request = UserCreateRequest("minnseong", null)
+
+        // when
+        userService.saveUser(request)
+
+        // then
+        val results = userRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("minnseong")
+        assertThat(results[0].age).isNull()
+    }
+}
