c0f57bbab3e482c8cea00979d1217fad06ed7a54
minseong <kdsvip5@naver.com>
2025-08-17 16:51:55 +0900

    [practical-testing] initial commit

---
+++ practical-testing/src/main/java/com/example/practical_testing/PracticalTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.practical_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalTestingApplication.class, args);
+	}
+
+}
